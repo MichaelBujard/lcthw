2375395db9ec471b89de87b0770776ece368a69c
MichaelBujard <mbujard@udallas.edu>
2021-09-21 23:01:42 +0100

    add updates

---
+++ code/ex9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    int j = argc;
+
+    while (i < 25) {
+        printf("%d\n", i);
+	
+	// do more stuff here
+	
+	
+	i++;
+    }
+
+    return 0;
+}
