46b9a930014679fa047c402f98d7aae13521223f
MichaelBujard <mbujard@udallas.edu>
2021-09-22 03:31:14 +0100

    commit edited ex9.c

--- code/ex9.c
+++ code/ex9.c
@@ -6,14 +6,45 @@ int main(int argc, char *argv[])
 
     int j = argc;
 
-    while (i < 25) {
-        printf("%d\n", i);
-	
-	// do more stuff here
-	
-	
+    char *state;
+
+    while (i < j) {
+        printf("arg %d: %s\n", i, argv[i]);
 	i++;
     }
+
+    while (i < (j + 4)) {
+	
+	switch ((i - j)) {
+	    case 0:
+		state = "California";
+                break;
+	    case 1:
+		state = "Oregon";
+		break;
+	    case 2:
+		state = "Washington";
+		break;
+            case 3:
+                state = "Texas";
+		break;
+	    default:
+		printf("\nERROR in switch\n");
+	}
+
+	printf("state %i: %s\n", (i - j), state);
+
+        i++;
+    }
+
+    printf("----------------------------\n");
+    printf(" while loop: counting down.\n");
+    i = 25;
+    while (i >= 0) {
+        printf("%d\n", i);
+        i--;
+    }
+    printf("----------------------------\n");
 
     return 0;
 }
